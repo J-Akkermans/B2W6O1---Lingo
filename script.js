b4b7d6c111eabdf9322926b76dea57476f81dbeb
J Akkermans <99044668@mydavinci.nl>
2021-01-15 09:25:12 +0100

    yes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var randomWord = Math.floor(Math.random() * words.length);
-var woord = words[randomWord].split('');
+var woord = words[0].split('');
+var copyWord;
 for (var i = 0; i < woord.length; i++) {
     woordLetter = document.createElement('p');
     woordLetter.id = i;
@@ -9,21 +10,25 @@ for (var i = 0; i < woord.length; i++) {
     userInput.placeholder = woord[0]
     document.getElementById('0').innerHTML = woord[0]
 }
-var counter = 0;
-function check(){
-    counter++;
-    let userInput = document.getElementById('userInput').value;
+function check(){  
+    let userInput = document.getElementById('userInput').value; 
     userInput = userInput.split('');
-    for(i = 0; i < woord.length; i++){
-        userInput.placeholder = woord[0] + "test"
-        let outCome = document.getElementById(i);
-        outCome.innerHTML = userInput[i]
-        if(woord[i] == woord[i]){
+    copyWord = words[0].split('');
+    for (i = 0; i < woord.length; i++) {
+        var outCome = document.getElementById(i);
+        outCome.innerText = userInput[i]
+        if (copyWord[i] === userInput[i]) {
             outCome.style.color = "Green"
-        }else if (woord.includes(userInput[i])) {
-            outCome.style.color = 'Orange'
-        }else if (!outCome.includes(userInput[i])) {
-            outCome.style.color = 'Grey'
+            copyWord[i] = ''
+        }
+    }
+    for (i = 0; i < woord.length; i++) {
+        console.log(copyWord, userInput[i])
+        if (copyWord.includes(userInput[i])) {
+            console.log(woord.indexOf(userInput[i]))
+            copyWord[woord.indexOf(userInput[i])] = ''
+            outCome.style.color = "Orange"
+            
         }
     }
 }
