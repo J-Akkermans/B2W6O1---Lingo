d5097dc35053661bf61c7bb4aab23cfb8ca8a5a9
J Akkermans <99044668@mydavinci.nl>
2021-01-17 01:07:14 +0100

    fix

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-var randomWord = Math.floor(Math.random() * words.length);
-var woord = words[0].split('');
-var copyWord;
-for (var i = 0; i < woord.length; i++) {
-    woordLetter = document.createElement('p');
-    woordLetter.id = i;
-    woordLetter.style.display = "inline"
-    woordLetter.innerText = i;
-    letterSection.appendChild(woordLetter);
-    userInput.placeholder = woord[0]
-    document.getElementById('0').innerHTML = woord[0]
-}
-function check(){  
-    let userInput = document.getElementById('userInput').value; 
-    userInput = userInput.split('');
-    copyWord = words[0].split('');
-    for (i = 0; i < woord.length; i++) {
-        var outCome = document.getElementById(i);
-        outCome.innerText = userInput[i]
-        if (copyWord[i] === userInput[i]) {
-            outCome.style.color = "Green"
-            copyWord[i] = ''
+var randomWord = words[Math.floor(Math.random() * words.length)];
+var word = [...words[0].split('')];
+var storageWord = [... words[0].split('')];
+var guess = 0;
+
+function lingo(){
+    // Maak boxes aan om letters in te stoppen.
+    var list = document.createElement("ul");
+    list.id = "guess " + guess
+    document.getElementById('wordlist').appendChild(list)
+    for (i=0;i<5;i++){
+        var item = document.createElement("li");
+        item.id = i
+        document.getElementById("guess " + guess).appendChild(item)
+    }
+    guess++
+
+    //Lingo logica. 
+    var inputWord = document.getElementById("inputWord").value;
+    inputWord = inputWord.split("")
+    for(let i = 0; i <word.length; i++){
+        document.getElementById(i).innerHTML = inputWord[i];
+        document.getElementById(i).style.backgroundColor = "white"
+        if (storageWord[i] === inputWord[i]) {
+            document.getElementById(i).style.backgroundColor = "Green"
+            storageWord[i] = ''
         }
     }
-    for (i = 0; i < woord.length; i++) {
-        console.log(copyWord, userInput[i])
-        if (copyWord.includes(userInput[i])) {
-            console.log(woord.indexOf(userInput[i]))
-            copyWord[woord.indexOf(userInput[i])] = ''
-            outCome.style.color = "Orange"
-            
+    for (i = 0; i < storageWord.length; i++) {
+        if (storageWord.includes(inputWord[i])) {
+            document.getElementById(i).style.backgroundColor = "Orange"
+            storageWord[word.indexOf(inputWord[i])] = ''
         }
     }
+    for (i=0;i<5;i++){
+        document.getElementById(i).id = ""
+    }
+    storageWord = [...word]
 }
+
+
+
+
+
