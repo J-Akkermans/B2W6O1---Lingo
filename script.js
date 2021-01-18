3c0ed3c40471e2b601bbc83e4081404dd7e1eaa1
J Akkermans <99044668@mydavinci.nl>
2021-01-18 11:20:55 +0100

    ahahaha

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 var randomWord = words[Math.floor(Math.random() * words.length)];
 var word = [...words[0].split('')];
-var storageWord = [... words[0].split('')];
+var storageWord = [...words[0].split('')];
 var guess = 0;
 
-function lingo(){
-    // Maak boxes aan om letters in te stoppen.
-    var list = document.createElement("ul");
-    list.id = "guess " + guess
-    document.getElementById('wordlist').appendChild(list)
-    for (i=0;i<5;i++){
-        var item = document.createElement("li");
-        item.id = i
-        document.getElementById("guess " + guess).appendChild(item)
-    }
-    guess++
+// Maak boxes aan om letters in te stoppen.
+addBoxes()
+function addBoxes(){
+var list = document.createElement("ul");
+list.id = "guess " + guess
+document.getElementById('wordlist').appendChild(list)
+for (i = 0; i < 5; i++) {
+    var item = document.createElement("li");
+    item.id = i
+    document.getElementById("guess " + guess).appendChild(item)
+}
+document.getElementById(0).innerHTML = word[0]
+guess++
+}
 
+function lingo() {
+    addBoxes()
     //Lingo logica. 
     var inputWord = document.getElementById("inputWord").value;
     inputWord = inputWord.split("")
-    for(let i = 0; i <word.length; i++){
+    for (let i = 0; i < word.length; i++) {
         document.getElementById(i).innerHTML = inputWord[i];
         document.getElementById(i).style.backgroundColor = "white"
         if (storageWord[i] === inputWord[i]) {
@@ -32,13 +37,8 @@ function lingo(){
             storageWord[word.indexOf(inputWord[i])] = ''
         }
     }
-    for (i=0;i<5;i++){
+    for (i = 0; i < 5; i++) {
         document.getElementById(i).id = ""
     }
     storageWord = [...word]
 }
-
-
-
-
-
