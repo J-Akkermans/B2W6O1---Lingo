13913b8641a5070d9c3b198ecdb743dd0a412d19
J Akkermans <99044668@mydavinci.nl>
2021-01-21 14:39:15 +0100

    aaa

--- script.js
+++ script.js
@@ -2,24 +2,25 @@ var randomWord = words[Math.floor(Math.random() * words.length)];
 var word = [...words[0].split('')];
 var storageWord = [...words[0].split('')];
 var guess = 0;
-
+var counter = 0;
 // Maak boxes aan om letters in te stoppen.
 addBoxes()
-function addBoxes(){
-var list = document.createElement("ul");
-list.id = "guess " + guess
-document.getElementById('wordlist').appendChild(list)
-for (i = 0; i < 5; i++) {
-    var item = document.createElement("li");
-    item.id = i
-    document.getElementById("guess " + guess).appendChild(item)
+function addBoxes() {
+    counter++
+    console.log(counter)
+    var list = document.createElement("ul");
+    list.id = "guess " + guess
+    document.getElementById('wordlist').appendChild(list)
+    for (i = 0; i < 5; i++) {
+        var item = document.createElement("li");
+        item.id = i
+        document.getElementById("guess " + guess).appendChild(item)
+    }
+    document.getElementById(0).innerHTML = word[0]
+    document.getElementById(0).style.BackgroundColor = "Green"
+    guess++
 }
-document.getElementById(0).innerHTML = word[0]
-guess++
-}
-
 function lingo() {
-    
     //Lingo logica. 
     var inputWord = document.getElementById("inputWord").value;
     inputWord = inputWord.split("")
@@ -37,9 +38,25 @@ function lingo() {
             storageWord[word.indexOf(inputWord[i])] = ''
         }
     }
-    for (i = 0; i < 5; i++) {
-        document.getElementById(i).id = ""
+    if (storageWord.every(element => element === '')&& counter != 5) {
+        setTimeout(function () {
+            var img = document.createElement("img")
+            img.src = "media/win.gif"
+            document.body.appendChild(img)
+            setTimeout(function () {
+                img.remove()
+                location.reload()
+            }, 15000);
+        }, 1000);
+        var audio = new Audio('media/win.mp3');
+        audio.play();
+    }else if(counter == 5){
+
+    }else {
+        for (i = 0; i < 5; i++) {
+            document.getElementById(i).id = ""
+            storageWord = [...word]
+            addBoxes()
+        }
     }
-    storageWord = [...word]
-    addBoxes()
 }
